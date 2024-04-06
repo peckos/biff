90fad0c2053462d96d0972823bac6ef88f7cc068
Paweł Koziołko <pawel.koziolko@ttsw.com.pl>
2024-04-06 14:27:58 +0200

    Dodanie nowego modułu

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  constructor(private router: Router){}
+
+  signIn() {
+    // TODO wysyłanie requestu do backedu o zweryfikowanie loginu i hasła
+    this.router.navigate(['/users']);
+  }
 
 }
