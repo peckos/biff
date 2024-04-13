8b56ea4fc91aacc8288d2ece9fd636516e4bb14f
Paweł Koziołko <pawel.koziolko@ttsw.com.pl>
2024-04-13 14:10:58 +0200

    Dodanie topbar

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent {
+export class ShellComponent implements OnInit {
+  items: MenuItem[] = [];
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        icon: 'pi pi-fw pi-file'
+      }
+    ];
+  }
 }
--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -6,10 +6,17 @@ import { ShellComponent } from './shell.component';
 import { MenuComponent } from './menu/menu.component';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
 
 
 @NgModule({
   declarations: [ShellComponent, MenuComponent],
-  imports: [CommonModule, RouterModule, ShellRoutingModule, PanelMenuModule]
+  imports: [
+    CommonModule,
+    RouterModule,
+    ShellRoutingModule,
+    PanelMenuModule,
+    MenubarModule
+  ]
 })
 export class ShellModule {}
